59c62370b7113c4398bb3ecd674d561e32f1cecf
Anicreat0r <153427492+Anicreat0r@users.noreply.github.com>
2026-09-20 05:26:44 +0530

    Delete anikatsu_by_Anicreator (1) directory

--- anikatsu_by_Anicreator (1)/[id].js
+++
@@ -1,9 +0,0 @@
-const UPSTREAM = "https://anikotoapi.site";
-
-export async function onRequestGet(context) {
-  const { id } = context.params;
-  const up = await fetch(`${UPSTREAM}/series/${encodeURIComponent(id)}`);
-  if (!up.ok) return new Response("Upstream error", { status: up.status });
-  const json = await up.json();
-  return Response.json(json);
-}
